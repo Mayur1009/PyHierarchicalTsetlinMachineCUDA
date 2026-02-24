0b54e6ba8e491517eb6952fc04dd71c7673ddf4e
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-02-24 14:12:16 +0100

    Update

--- PyHierarchicalTsetlinMachineCUDA/kernels.py
+++ PyHierarchicalTsetlinMachineCUDA/kernels.py
@@ -615,9 +615,6 @@ code_encode = """
 						int leaf_chunk_nr = k / 32;
 						int leaf_chunk_pos = k % 32;
 
-						if (index == 0) {
-							printf("%d\\n", j*number_of_literal_chunks_per_leaf + leaf_chunk_nr);
-						}
 						if (Xi[j*number_of_literals_per_leaf + k] == 1) {
 							//encoded_Xi[j*number_of_literal_chunks_per_leaf + leaf_chunk_nr] |= (1 << leaf_chunk_pos);
 						}
