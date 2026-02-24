4c16fff06722820c2f69eea48c0d1e5d25ed0354
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-02-24 14:05:55 +0100

    Update

--- PyHierarchicalTsetlinMachineCUDA/kernels.py
+++ PyHierarchicalTsetlinMachineCUDA/kernels.py
@@ -609,17 +609,20 @@ code_encode = """
 
 			for (unsigned long long i = index; i < number_of_examples; i += stride) {
 				Xi = &X[i*number_of_literals];
-				/*encoded_Xi = &encoded_X[i*number_of_literal_chunks];
+				encoded_Xi = &encoded_X[i*number_of_literal_chunks];
 				for (int j = 0; j < number_of_leaves; ++j) {
 					for (int k = 0; k < number_of_literals_per_leaf; ++k) {
 						int leaf_chunk_nr = k / 32;
 						int leaf_chunk_pos = k % 32;
 
-						if (Xi[j*number_of_literals_per_leaf + k] == 1) {
-							encoded_Xi[j*number_of_literal_chunks_per_leaf + leaf_chunk_nr] |= (1 << leaf_chunk_pos);
+						if (index == 0) {
+							printf("%d\\n", j*number_of_literal_chunks_per_leaf + leaf_chunk_nr);
 						}
+						/*if (Xi[j*number_of_literals_per_leaf + k] == 1) {
+							encoded_Xi[j*number_of_literal_chunks_per_leaf + leaf_chunk_nr] |= (1 << leaf_chunk_pos);
+						}*/
 					}
-				}*/
+				}
 			} 
 		}
 	}
