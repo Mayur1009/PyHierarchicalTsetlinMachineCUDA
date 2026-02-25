0a5ba7e355b22b2fac3faeb94fd140d8e5745a5d
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-02-25 16:12:30 +0100

    Update

--- PyHierarchicalTsetlinMachineCUDA/kernels.py
+++ PyHierarchicalTsetlinMachineCUDA/kernels.py
@@ -225,7 +225,7 @@ code_update = """
 					int ta_pos_hierarchy = leaf_literal % 32;
 
 					for (int l = 0; l < STATE_BITS; ++l) {
-						if ((ta_state_flat[ta_chunk_flat*STATE_BITS + l] & (1 << ta_pos_flat)) == 1) {
+						if ((ta_state_flat[ta_chunk_flat*STATE_BITS + l] & (1 << ta_pos_flat)) > 0) {
 							ta_state_hierarchy[leaf*TA_CHUNKS_PER_LEAF*STATE_BITS + ta_chunk_hierarchy*STATE_BITS + l] |= (1 << ta_pos_hierarchy);
 						} else {
 							ta_state_hierarchy[leaf*TA_CHUNKS_PER_LEAF*STATE_BITS + ta_chunk_hierarchy*STATE_BITS + l] &= ~(1 << ta_pos_hierarchy);
