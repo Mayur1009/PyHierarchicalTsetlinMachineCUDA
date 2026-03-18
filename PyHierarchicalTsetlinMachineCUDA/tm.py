1b31b70b37f182086174abd05f9f4419d0e80e70
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-03-18 10:10:22 +0100

    Update

--- PyHierarchicalTsetlinMachineCUDA/tm.py
+++ PyHierarchicalTsetlinMachineCUDA/tm.py
@@ -411,7 +411,8 @@ class CommonTsetlinMachine():
 				self.evaluate_final.prepared_call(self.grid, self.block, self.hierarchy_votes[self.depth-1], self.clause_weights_gpu, self.class_sum_gpu)
 				cuda.Context.synchronize()
 
-				for d in range(1, self.depth):
+				for d in range(self.depth-1, 0, -1):
+					print("DEPTH PROP %d" % (d))
 					self.propagate_and_group_false_truth_values.prepared_call(self.grid, self.block, self.hierarchy_votes[d-1], self.hierarchy_votes[d], self.hierarchy_size[d + 1], self.hierarchy_structure[d][1])
 					cuda.Context.synchronize()
 				
