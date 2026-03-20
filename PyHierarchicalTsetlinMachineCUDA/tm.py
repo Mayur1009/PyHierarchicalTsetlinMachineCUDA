3559918a5063f195ffbb08d5dc22695d4cc3c45d
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-03-20 13:34:13 +0100

    Update

--- PyHierarchicalTsetlinMachineCUDA/tm.py
+++ PyHierarchicalTsetlinMachineCUDA/tm.py
@@ -385,8 +385,8 @@ class CommonTsetlinMachine():
 				class_sum = np.ascontiguousarray(np.zeros(self.number_of_outputs)).astype(np.int32)
 				cuda.memcpy_htod(self.class_sum_gpu, class_sum)
 
-				self.convert_ta_states.prepared_call(self.grid, self.block, self.ta_state_gpu, self.ta_state_hierarchy_gpu)
-				cuda.Context.synchronize()
+				#self.convert_ta_states.prepared_call(self.grid, self.block, self.ta_state_gpu, self.ta_state_hierarchy_gpu)
+				#cuda.Context.synchronize()
 
 				#self.compare_ta_states.prepared_call(self.grid, self.block, self.ta_state_gpu, self.ta_state_hierarchy_gpu)
 				#cuda.Context.synchronize()
@@ -495,8 +495,8 @@ class CommonTsetlinMachine():
 
 			cuda.memcpy_htod(self.class_sum_gpu, class_sum_example)
 			
-			self.convert_ta_states.prepared_call(self.grid, self.block, self.ta_state_gpu, self.ta_state_hierarchy_gpu)
-			cuda.Context.synchronize()
+			#self.convert_ta_states.prepared_call(self.grid, self.block, self.ta_state_gpu, self.ta_state_hierarchy_gpu)
+			#cuda.Context.synchronize()
 
 			#self.compare_ta_states.prepared_call(self.grid, self.block, self.ta_state_gpu, self.ta_state_hierarchy_gpu)
 			#cuda.Context.synchronize()
