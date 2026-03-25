4f7fa451f3aab55db3a151cdb042a984274058fe
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-03-25 16:46:31 +0100

    Update

---
+++ examples/MNISTDemo.py
@@ -0,0 +1,35 @@
+from PyHierarchicalTsetlinMachineCUDA.tm import MultiClassTsetlinMachine
+import numpy as np
+from time import time
+import PyHierarchicalTsetlinMachineCUDA.tm as tm
+from keras.datasets import mnist
+
+factor = 1
+
+s = 10.0
+
+T = int(factor*5)*4096
+
+ensembles = 10
+epochs = 250
+
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+
+X_train = np.where(X_train.reshape((X_train.shape[0], 28*28)) > 75, 1, 0)#[0:1000]
+Y_train = Y_train#[0:1000]
+X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0)#[0:1000]
+Y_test = Y_test#[0:1000]
+
+tm = MultiClassTsetlinMachine(int(factor*2000), T, s, hierarchy_structure=((tm.AND_GROUP, 28*7), (tm.OR_ALTERNATIVES, 8), (tm.AND_GROUP, 4)))
+
+print("\nAccuracy over 500 epochs:\n")
+for i in range(500):
+	start_training = time()
+	tm.fit(X_train, Y_train, epochs=1, incremental=True)
+	stop_training = time()
+
+	start_testing = time()
+	result = 100*(tm.predict(X_test) == Y_test).mean()
+	stop_testing = time()
+
+	print("#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result, stop_training-start_training, stop_testing-start_testing))
