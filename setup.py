f118a4cf8d0c884ec989c5ef1d32e259a2174645
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-03-27 17:02:23 +0100

    Add install_requires and extras_require to setup.py
    Added dependencies for numpy, pycuda, scipy, and scikit-learn.

--- setup.py
+++ setup.py
@@ -11,4 +11,13 @@ setup(
    long_description='Hierarchical Tsetlin Machine Architecture.',
    keywords ='pattern-recognition cuda machine-learning interpretable-machine-learning rule-based-machine-learning propositional-logic tsetlin-machine regression convolution classification multi-layer',
    packages=['PyHierarchicalTsetlinMachineCUDA']
+   install_requires=[
+		'numpy',
+		'pycuda',
+		'scipy',
+		'scikit-learn',
+	],
+	extras_require={
+		'examples': ['tensorflow'],
+	}
 )
