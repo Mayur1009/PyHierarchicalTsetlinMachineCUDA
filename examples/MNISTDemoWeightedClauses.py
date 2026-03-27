c225e796dad65687afdb41ccae5d7faa017207ae
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-03-27 12:36:00 +0100

    Update

--- examples/MNISTDemoWeightedClauses.py
+++ examples/MNISTDemoWeightedClauses.py
@@ -5,15 +5,15 @@ import PyHierarchicalTsetlinMachineCUDA.tm as tm
 from keras.datasets import mnist
 
 clauses = 2000
-T = 50*10*10000
-s = 20.0
+T = 50*10*625
+s = 10.0
 
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
 X_train = np.where(X_train.reshape((X_train.shape[0], 28*28)) > 75, 1, 0)
 X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0)
 
-tm = MultiClassTsetlinMachine(clauses, T, s, weighted_clauses=True, hierarchy_structure=((tm.AND_GROUP, 28*7), (tm.OR_ALTERNATIVES, 10), (tm.AND_GROUP, 4)))
+tm = MultiClassTsetlinMachine(clauses, T, s, weighted_clauses=True, hierarchy_structure=((tm.AND_GROUP, 28*7), (tm.OR_ALTERNATIVES, 5), (tm.AND_GROUP, 4)))
 
 print("\nAccuracy over 500 epochs:\n")
 for i in range(500):
