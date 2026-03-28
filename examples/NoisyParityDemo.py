c20c94806a7802392e878c0afb22c8fd4dcdcc45
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-03-28 19:31:59 +0100

    Update

--- examples/NoisyParityDemo.py
+++ examples/NoisyParityDemo.py
@@ -4,7 +4,7 @@ from time import time
 import PyHierarchicalTsetlinMachineCUDA.tm as tm
 
 clauses = 32
-s = 20.1
+s = 15.0
 T = 625
 
 train_data = np.loadtxt("./examples/NoisyParityTrainingData.txt").astype(np.uint32)
