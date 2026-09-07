f6fa00a92b1df96179a4f4c30d90761fcc4ee509
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-09-07 16:20:00 +0200

    Update

---
+++ examples/MNISTXORDemo.py
@@ -0,0 +1,53 @@
+from PyHierarchicalTsetlinMachineCUDA.tm import MultiClassTsetlinMachine
+import numpy as np
+from time import time
+import PyHierarchicalTsetlinMachineCUDA.tm as tm
+from keras.datasets import mnist
+
+clauses = 4
+T = 50
+s = 10.0
+
+number_of_training_examples = 10000
+number_of_testing_examples = 1000
+
+(X_train_mnist, Y_train_mninst), (X_test_mnist, Y_test_mnist) = mnist.load_data()
+
+X_train_mnist = np.where(X_train_mnist.reshape((X_train_mnist.shape[0], 28*28)) > 75, 1, 0)
+X_test_mnist = np.where(X_test_mnist.reshape((X_test_mnist.shape[0], 28*28)) > 75, 1, 0)
+
+X_train = np.empty((number_of_training_examples, 28*28*2))
+Y_train = np.empty((number_of_training_examples, 28*28*2))
+for i in range(number_of_training_examples):
+	x = np.random.randint(2, size=(2))
+
+	X_train[i,:28*28] = X_train_mnist[Y_mnist_train == x[0]][0]
+	X_train[i,28*28_] = X_train_mnist[Y_mnist_train == x[1]][0]	
+
+	Y_train[i] = np.logical_xor(x[0] % 2, x[1] % 2)
+
+
+X_test = np.empty((number_of_testing_examples, 28*28*2))
+Y_test = np.empty((number_of_testing_examples, 28*28*2))
+for i in range(number_of_testing_examples):
+	x = np.random.randint(2, size=(2))
+
+	X_test[i,:28*28] = X_test_mnist[Y_mnist_test == x[0]][0]
+	X_test[i,28*28:] = X_test_mnist[Y_mnist_test == x[1]][0]	
+
+	Y_test[i] = np.logical_xor(x[0] % 2, x[1] % 2)
+
+tm = MultiClassTsetlinMachine(clauses, T, s, hierarchy_structure=((tm.AND_GROUP, 28*28), (tm.OR_ALTERNATIVES, 2000), (tm.AND_GROUP, 2)))
+
+print("\nAccuracy over 500 epochs:\n")
+for i in range(500):
+	start_training = time()
+	tm.fit(X_train, Y_train)
+	stop_training = time()
+
+
+	start_testing = time()
+	result = 100*(tm.predict(X_test) == Y_test).mean()
+	stop_testing = time()
+
+	print("#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result, stop_training-start_training, stop_testing-start_testing))
