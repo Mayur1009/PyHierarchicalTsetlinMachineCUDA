7b8f75c1f142a51d03374bd5f073fb1dbc392191
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-09-09 12:47:00 +0200

    Update

--- examples/ANDOfXORsDemo.py
+++ examples/ANDOfXORsDemo.py
@@ -119,18 +119,22 @@ for i in range(10):
 		)
 
 	start_training = time()
+	max_result = 0.0
 	for e in range(args.epochs):
 		tsetlin_machine.fit(X_train, Y_train)
+
+		start_testing = time()
+		result = 100*(tsetlin_machine.predict(X_test) == Y_test).mean()
+		if result > max_result:
+			max_result = max_result
+		stop_testing = time()
+
 	stop_training = time()
 
-	start_testing = time()
-	result = 100*(tsetlin_machine.predict(X_test) == Y_test).mean()
-	stop_testing = time()
-
-	average_result += result / 10.0
+	average_result += max_result / 10.0
 
 	tsetlin_machine.print_hierarchy(print_ta_state=True)
 
-	print("\n#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result, stop_training-start_training, stop_testing-start_testing))
+	print("\n#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, max_result, stop_training-start_training, stop_testing-start_testing))
 
 print("\nAverage Accuracy: %.2f%%" % (average_result,))
