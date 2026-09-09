ff3f1657df2148f0860cf8a230b65857e575fd7f
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-09-09 10:54:18 +0200

    Update

--- examples/ANDOfXORsDemo.py
+++ examples/ANDOfXORsDemo.py
@@ -8,7 +8,7 @@ def default_args(**kwargs):
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--epochs", default=1000, type=int)
 	parser.add_argument("--number-of-clauses", default=2, type=int)
-	parser.add_argument("--number-of-irrelevant-features", default=2, type=int)
+	parser.add_argument("--number-of-irrelevant-features", default=1, type=int)
 	parser.add_argument("--number-of-examples", default=10000, type=int)
 	parser.add_argument("--T", default=20, type=int)
 	parser.add_argument("--s", default=2.1, type=float)
