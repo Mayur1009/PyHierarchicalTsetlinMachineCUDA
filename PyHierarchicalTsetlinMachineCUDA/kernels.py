6dc3377bf9d70deeabb0d3fe5a97c1422607c4ef
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-09-18 14:05:54 +0200

    Update

--- PyHierarchicalTsetlinMachineCUDA/kernels.py
+++ PyHierarchicalTsetlinMachineCUDA/kernels.py
@@ -94,9 +94,9 @@ code_update = """
 
 			int sign = (*clause_weight >= 0) - (*clause_weight < 0);
 		
-			#if CONSTANT_UPDATE_PROBABILITY == 1
+			#if CONSTANT_UPDATE_P == 1
 				printf("CONSTANT UPDATE P\\n");
-				float update_probability = CONSTANT_UPDATE_PROBABILITY
+				float update_probability = 0.1
 			#else
 				float update_probability = fabsf(y - class_sum)/(2*THRESHOLD);
 			#endif
@@ -132,7 +132,7 @@ code_update = """
 		
 			#if CONSTANT_UPDATE_PROBABILITY == 1
 				printf("CONSTANT UPDATE P\\n");
-				float update_probability = 1.0;
+				float update_probability = 0.1;
 			#else
 				float update_probability = fabsf(y - class_sum)/(2*THRESHOLD);
 			#endif
