96969d0dcfa5af23b40bc0f1b47d73ad3027c462
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-09-18 13:57:49 +0200

    Update

--- PyHierarchicalTsetlinMachineCUDA/tm.py
+++ PyHierarchicalTsetlinMachineCUDA/tm.py
@@ -142,6 +142,8 @@ class CommonTsetlinMachine():
 	#define FLIP_POLARITY %d
 		""" % (self.number_of_clauses, self.depth, self.hierarchy_size[1], self.number_of_ta_teams, self.number_of_literals_per_leaf, self.number_of_literal_chunks_per_leaf, self.number_of_literal_chunks, self.number_of_state_bits, self.boost_true_positive_feedback, self.s, self.T, self.q, self.binary_inference, self.constant_update_p, self.and_group_normalization, AND_GROUP, AND_ALTERNATIVES, OR_GROUP, OR_ALTERNATIVES, self.negative_clauses, self.flip_polarity)
 		
+		print(self.constant_update_p)
+		
 		mod_prepare = SourceModule(parameters + kernels.code_header + kernels.code_prepare, no_extern_c=True)
 		self.prepare_weights = mod_prepare.get_function("prepare_weights")
 		self.prepare_hierarchy = mod_prepare.get_function("prepare_hierarchy")
