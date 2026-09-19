b464a201b3892f162e010b9f8dcb5ea469bf2ee2
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-09-19 10:07:36 +0200

    Update

--- examples/NoisyParityDemo.py
+++ examples/NoisyParityDemo.py
@@ -7,6 +7,7 @@ import argparse
 def default_args(**kwargs):
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--epochs", default=1000, type=int)
+	parser.add_argument("--runs", default=10, type=int)
 	parser.add_argument("--number-of-clauses", default=32, type=int)
 	parser.add_argument("--number-of-state-bits", default=7, type=int)
 	parser.add_argument("--T", default=250, type=int)
@@ -34,29 +35,31 @@ test_data = np.loadtxt("./examples/NoisyParityTestingData.txt").astype(np.uint32
 X_test = test_data[:,0:-1]
 Y_test = test_data[:,-1]
 
-seed = np.random.randint(10000)
-if args.vanilla:
-	tm = TsetlinMachine(args.number_of_clauses * args.number_of_alternatives_1 * args.number_of_alternatives_2, args.T, args.s, binary_inference=args.binary_inference, constant_update_p=args.constant_update_p, and_group_normalization=args.and_group_normalization, seed=seed, number_of_state_bits=args.number_of_state_bits, boost_true_positive_feedback=0, hierarchy_structure=((tm.AND_GROUP, 12), (tm.AND_GROUP, 1)))
-else:
-	tm = TsetlinMachine(args.number_of_clauses, args.T, args.s, binary_inference=args.binary_inference, constant_update_p=args.constant_update_p, and_group_normalization=args.and_group_normalization, seed=seed, number_of_state_bits=args.number_of_state_bits, boost_true_positive_feedback=0, hierarchy_structure=((tm.AND_GROUP, 3), (tm.OR_ALTERNATIVES, args.number_of_alternatives_1), (tm.AND_GROUP, 2), (tm.OR_ALTERNATIVES, args.number_of_alternatives_2), (tm.AND_GROUP, 2)))
-
-f = open("statistics_%d_%d_%.2f_%d_%d_%d_%d_%d_%d.txt" % (args.number_of_clauses, args.T, args.s, args.number_of_state_bits, args.vanilla, args.and_group_normalization, seed, args.constant_update_p, args.binary_inference), "w")
+f = open("statistics_%d_%d_%.2f_%d_%d_%d_%d_%d.txt" % (args.number_of_clauses, args.T, args.s, args.number_of_state_bits, args.vanilla, args.and_group_normalization, args.constant_update_p, args.binary_inference), "w")
 
 print("\nAccuracy over %d epochs:\n" % (args.epochs,))
-for e in range(args.epochs):
-	start_training = time()
-	tm.fit(X_train, Y_train)
-	stop_training = time()
 
-	start_testing = time()
-	result = 100*(tm.predict(X_test) == Y_test).mean()
-	stop_testing = time()
+for r in range(args.runs):
+	seed = np.random.randint(10000)
+	if args.vanilla:
+		tsetlin_machine = TsetlinMachine(args.number_of_clauses * args.number_of_alternatives_1 * args.number_of_alternatives_2, args.T, args.s, binary_inference=args.binary_inference, constant_update_p=args.constant_update_p, and_group_normalization=args.and_group_normalization, seed=seed, number_of_state_bits=args.number_of_state_bits, boost_true_positive_feedback=0, hierarchy_structure=((tm.AND_GROUP, 12), (tm.AND_GROUP, 1)))
+	else:
+		tsetlin_machine = TsetlinMachine(args.number_of_clauses, args.T, args.s, binary_inference=args.binary_inference, constant_update_p=args.constant_update_p, and_group_normalization=args.and_group_normalization, seed=seed, number_of_state_bits=args.number_of_state_bits, boost_true_positive_feedback=0, hierarchy_structure=((tm.AND_GROUP, 3), (tm.OR_ALTERNATIVES, args.number_of_alternatives_1), (tm.AND_GROUP, 2), (tm.OR_ALTERNATIVES, args.number_of_alternatives_2), (tm.AND_GROUP, 2)))
 
-	tm.print_hierarchy()
+	for e in range(args.epochs):
+		start_training = time()
+		tsetlin_machine .fit(X_train, Y_train)
+		stop_training = time()
 
-	print("\n#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (e+1, result, stop_training-start_training, stop_testing-start_testing))
+		start_testing = time()
+		result = 100*(tm.predict(X_test) == Y_test).mean()
+		stop_testing = time()
 
-	f.write("%d %.2f\n" % (e, result))
-	f.flush()
+		tsetlin_machine.print_hierarchy()
+
+		print("\n#%d/%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (r+1, e+1, result, stop_training-start_training, stop_testing-start_testing))
+
+		f.write("%d %d %.2f\n" % (r, e, result))
+		f.flush()
 
 f.close()
