0177349c0cbac4699f0806ea252cb4b9d2245714
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-09-20 18:58:54 +0200

    Update

--- examples/MultiConceptDemo.py
+++ examples/MultiConceptDemo.py
@@ -7,6 +7,7 @@ import argparse
 def default_args(**kwargs):
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--epochs", default=1000, type=int)
+	parser.add_argument("--runs", default=10, type=int)
 	parser.add_argument("--number-of-clauses", default=2, type=int)
 	parser.add_argument("--number-of-state-bits", default=7, type=int)
 	parser.add_argument("--number-of-examples", default=10000, type=int)
