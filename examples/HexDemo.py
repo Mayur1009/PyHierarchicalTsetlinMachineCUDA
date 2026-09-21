331e015902ea149454c936684fba20c112042fd6
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-09-21 11:24:21 +0200

    Update

--- examples/HexDemo.py
+++ examples/HexDemo.py
@@ -6,8 +6,8 @@ import argparse
 
 def default_args(**kwargs):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--epochs", default=1000, type=int)
-    parser.add_argument("--runs", default=1000, type=int)
+    parser.add_argument("--epochs", default=10, type=int)
+    parser.add_argument("--runs", default=10, type=int)
     parser.add_argument("--number-of-clauses", default=2000, type=int)
     parser.add_argument("--T", default=9000, type=int)
     parser.add_argument("--s", default=44.0, type=float)
