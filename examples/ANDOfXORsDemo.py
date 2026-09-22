fcc086725fc50938d9b88ae01044a63ac9d527a4
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-09-22 10:11:56 +0200

    Update

--- examples/ANDOfXORsDemo.py
+++ examples/ANDOfXORsDemo.py
@@ -6,7 +6,7 @@ import argparse
 
 def default_args(**kwargs):
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--epochs", default=10, type=int)
+	parser.add_argument("--epochs", default=100, type=int)
 	parser.add_argument("--runs", default=10, type=int)
 	parser.add_argument("--number-of-clauses", default=2, type=int)
 	parser.add_argument("--number-of-state-bits", default=8, type=int)
