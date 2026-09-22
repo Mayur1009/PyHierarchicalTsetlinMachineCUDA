141b4c69feae736aa2960290f9795af4704d7811
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-09-22 08:52:35 +0200

    Update

--- PyHierarchicalTsetlinMachineCUDA/tm.py
+++ PyHierarchicalTsetlinMachineCUDA/tm.py
@@ -440,7 +440,10 @@ class CommonTsetlinMachine():
 			cuda.memcpy_dtoh(self.class_sum, self.class_sum_gpu)
 			class_sum[:, e] = self.class_sum
 	
-		class_sum = np.clip(class_sum.reshape((self.number_of_outputs, number_of_examples)), -self.T, self.T)
+		if self.clip:
+			class_sum = np.clip(class_sum.reshape((self.number_of_outputs, number_of_examples)), -self.T, self.T)
+		else:
+			class_sum = class_sum.reshape((self.number_of_outputs, number_of_examples))
 
 		return class_sum
 
